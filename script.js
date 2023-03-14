955e27dfe55616f1ede76ef8233c3f89154cce9f
Eric O. Beestrum <eric@beestrum.com>
2023-03-14 15:18:59 -0500

    Autosizes content, added hover/draw state. Still rough.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 /* Last updated: 3/14/2023   */
 
-function makeGrid(e) {
-  console.log(e);
+//DECLARE VARIABLES
+const e = 64; //GRID SIZE
+
+//FUNCTION buildGrid();
+function buildGrid(e) {
+  const container = document.getElementById("container");
+  let winH = window.innerHeight;
+  let winW = window.innerWidth;
+
+  //SQUARE THE HEIGHT, WIDTH
+  if (winH >= winW) {
+    winH = winW;
+  } else {
+    winW = winH;
+  }
+
   for (let i = 0; i < e; i++) {
     //SETS ROWS & COLUMNS
     const row = document.createElement("div");
+    //DECLARE .row CLASS
     row.className = "row";
+    //ASSIGN # OF COLUMNS, COLUMN WIDTH IN ROWS
+    row.style.gridTemplateColumns = "repeat(" + e + ", " + winH / e + "px)";
+    //ASSIGN ROW HEIGHT
+    row.style.gridTemplateRows = winH / e + "px";
+
+    //SET COLUMNS IN EACH ROW
     for (let x = 0; x < e; x++) {
       const column = document.createElement("div");
       column.className = "column";
@@ -14,6 +35,13 @@ function makeGrid(e) {
     document.getElementById("container").appendChild(row);
   }
 }
+//END FUNCTION buildGrid();
 
-const e = 3; //DECLARE GRID SIZE
-window.onload = makeGrid(e); //RUN GRID
+window.onload = buildGrid(e); //RUN GRID
+
+//ETCH-A-SKETCH ON MOUSEOVER
+document.querySelectorAll(".column").forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+   item.style.backgroundColor = "red";
+  });
+});
